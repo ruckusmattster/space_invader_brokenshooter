a3148bba4742eac17d06cfa52935b15ecfda6c10
ruckusmattster <77196088+ruckusmattster@users.noreply.github.com>
2021-04-29 14:12:44 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,22 +2,25 @@ input.onButtonPressed(Button.A, function () {
 	
 })
 input.onPinPressed(TouchPin.P1, function () {
-    if (y_axis == 0) {
-        led.unplot(x_axis, y_axis)
+    if (y_axis != 0) {
+        led.unplot(player_x, y_axis)
         y_axis += -1
-        led.plot(x_axis, y_axis)
+        led.plot(player_x, y_axis)
         basic.pause(625)
     }
 })
 let y_axis = 0
-let x_axis = 0
-x_axis = 2
+let player_x = 0
+let x_axis = 2
+player_x = 2
+y_axis = 4
 led.plot(x_axis, 4)
 basic.forever(function () {
     while (input.isGesture(Gesture.TiltLeft)) {
         if (x_axis > 0) {
             led.unplot(x_axis, 4)
             x_axis += -1
+            player_x += 1
             led.plot(x_axis, 4)
         }
         basic.pause(625)
@@ -28,6 +31,7 @@ basic.forever(function () {
         if (x_axis < 4) {
             led.unplot(x_axis, 4)
             x_axis += 1
+            player_x += 1
             led.plot(x_axis, 4)
         }
         basic.pause(625)

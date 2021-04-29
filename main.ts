747b3412747722ce132d1d9a8d6eee3bb340f839
ruckusmattster <77196088+ruckusmattster@users.noreply.github.com>
2021-04-29 13:55:05 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (y_axis < 0) {
+        led.unplot(x_axis, y_axis)
+        y_axis += 1
+        x_axis += 1
+        led.plot(x_axis, y_axis)
+        basic.pause(625)
+    }
+})
+let y_axis = 0
+let x_axis = 0
+x_axis = 2
+led.plot(x_axis, 4)
+basic.forever(function () {
+    while (input.isGesture(Gesture.TiltLeft)) {
+        if (x_axis > 0) {
+            led.unplot(x_axis, 4)
+            x_axis += -1
+            led.plot(x_axis, 4)
+        }
+        basic.pause(625)
+    }
+})
+basic.forever(function () {
+    while (input.isGesture(Gesture.TiltRight)) {
+        if (x_axis < 4) {
+            led.unplot(x_axis, 4)
+            x_axis += 1
+            led.plot(x_axis, 4)
+        }
+        basic.pause(625)
+    }
+})

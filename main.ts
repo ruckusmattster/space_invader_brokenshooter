4aba5caf7bd5185e9a543fa7273762350259fa78
ruckusmattster <77196088+ruckusmattster@users.noreply.github.com>
2021-05-04 10:36:00 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,8 +14,23 @@ let missile_x = 0
 let missile_y = 0
 let player_x = 0
 player_x = 2
-player_x = 2
 led.plot(player_x, 4)
+let alien_x = 0
+let alien_y = 0
+while (true) {
+    for (let index = 0; index < 4; index++) {
+        led.plot(alien_x, alien_y)
+        basic.pause(500)
+        led.unplot(alien_x, alien_y)
+        alien_x += 1
+    }
+    for (let index = 0; index < 4; index++) {
+        led.plot(alien_x, alien_y)
+        basic.pause(500)
+        led.unplot(alien_x, alien_y)
+        alien_x += -1
+    }
+}
 // move left
 basic.forever(function () {
     while (input.isGesture(Gesture.TiltLeft)) {
